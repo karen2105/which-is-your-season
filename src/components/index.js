599345d4065c06d83131c0d28e2b8fd92dc80c76
karen2105 <ingkaren.alonso@gmail.com>
2019-08-29 20:35:31 +0100

    Get the season analising the location and date

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
 
 class Hello extends Component {
-  
+    constructor(props) {
+      super(props);
+
+      this.state = { lat: '', error: false, month: 0 };
+      this.determineSeason = this.determineSeason.bind(this);
+    }
+
+    componentDidMount() {
+      // Loading current position
+      navigator.geolocation.getCurrentPosition(
+        position => this.setState({lat: position.coords.latitude}),
+        error => this.setState({error: true}),
+      );
+
+      //Get date
+      const month = new Date().getMonth() + 1;
+      this.setState({month: month});
+    }
+
+    determineSeason(lat, month) {
+      let term = ''; 
+      let place = ''; 
+
+      //If latitud is greater than 0 (north)
+      // if it is during first term (months 3-8) and  = Summer
+      if(lat > 0) {
+        if(month > 3 && month <= 8) {
+          return 'summer';
+        } else {
+          return 'winter';
+        }
+      } 
+
+      //If latitud is less than 0 (south)
+      if(lat <= 0) {
+        if(month > 3 && month <= 8) {
+          return 'winter';
+        } else {
+          return 'summer';
+        }
+      }
+    }
+
     render() {
+      const season = this.determineSeason(this.state.lat, this.state.month);
+
         return (
-            <div>
-                Hola!!
+            <div className="which-season-wrapper">
+              {}
             </div>
         );
     }
