e90597d9d918c9d8a3370a07476dfb80a32afcd3
karen2105 <ingkaren.alonso@gmail.com>
2019-09-02 11:25:52 +0100

    Fixed problem with URL for videos, because didnt want to change

--- src/components/season-page/index.js
+++ src/components/season-page/index.js
@@ -1,68 +1,96 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import {SPRING, SUMMER, AUTUMM, WINTER} from '@/constants/seasons';
+
 import summerVideo from '@/assets/summer.mp4';
 import winterVideo from '@/assets/winter.mp4';
 import springVideo from '@/assets/spring.mp4';
 import autummVideo from '@/assets/autumm.mp4';
 
-function addButtons(removeSeason) {
-  const seasons = [SPRING, SUMMER, AUTUMM, WINTER];
-  const buttons = seasons
-    .filter(season => season !== removeSeason)
-    .map((seasonText, index) => <button key={`btn-${index}`}> {seasonText} </button>);
+class SeasonPage extends Component {
+  constructor(props) {
+    super(props);
 
-  return buttons;
-}
+    this.state = { 
+      currentSeason: this.props.currentSeason, 
+      country: this.props.country,
+      video: this.getVideoLink(this.props.currentSeason),
+    };
 
-const SeasonPage = ({currentSeason, country}) => {
-  let message = '';
-  let video = '';
-  let loopState = false;
-  const seasons = [SPRING, SUMMER, AUTUMM, WINTER];
+    this.handleClick = this.handleClick.bind(this);
+    this.getVideoLink = this.getVideoLink.bind(this);
+    this.addButtons = this.addButtons.bind(this);
 
-  if(seasons.includes(currentSeason)) {
-      message = `Amazing ${currentSeason} time in ${country == 'United Kingdom' ? `the ${country}` : country }, isn't it?.`;
-  } else {
-    message = "Oops, we don't where you are in this moment, but this is how British summer looks :D";
   }
 
-  switch(currentSeason){
-    case SPRING:
-      video = springVideo;
-      break;
-    case SUMMER:
-      video = summerVideo;
-      break;
-    case AUTUMM:
-      video = autummVideo;
-      loopState = true;
-      break;
-    case WINTER:
-      video = winterVideo;
-      break;
-    default:
-      video = summerVideo;
-      break;
+  getVideoLink(currentSeason) {
+    switch(currentSeason){
+      case SPRING:
+        return springVideo;
+      case SUMMER:
+        return summerVideo;
+      case AUTUMM:
+        return autummVideo;
+      case WINTER:
+        return winterVideo;
+      default:
+        return summerVideo;
+    }
   }
 
-  return (
-    <div className="which-season-container">
-      <div className="video-overlay-pattern" />
-      <video autoPlay muted loop={loopState} id="bgVideo" className="bgVideo">
-        <source src={video} type="video/mp4" />
-      </video>
+  addButtons(removeSeason) {
+    const seasons = [SPRING, SUMMER, AUTUMM, WINTER];
+    const buttons = seasons
+      .filter(season => season !== removeSeason)
+      .map((seasonText, index) => <button onClick={(e) => this.handleClick(seasonText)} key={`btn-${index}`}> {seasonText} </button>);
 
-      <div className="season-page-message">
-        <span className="message-min-text">{message}</span>
-        <div className="season-page-buttons-wrapper">
-          <p>Check out other seasons</p>
-          {addButtons(currentSeason)}
+    return buttons;
+  }
+
+  handleClick(season) {
+    this.setState({
+      currentSeason: season, 
+      video: this.getVideoLink(season)
+    });
+  }
+
+  render() {
+    const seasons = [SPRING, SUMMER, AUTUMM, WINTER];
+    const {currentSeason, country} = this.state;
+    let message = '';
+    let loopState = currentSeason !== 'autumm' ? false : true;
+
+    if(seasons.includes(currentSeason)) {
+        message = `Amazing ${currentSeason} time in ${country == 'United Kingdom' ? `the ${country}` : country }, isn't it?.`;
+    } else {
+      message = "Oops, we don't where you are in this moment, but this is how British summer looks :D";
+    }
+
+    return (
+      <div className="which-season-container">
+        <div className="video-overlay-pattern" />
+        <video 
+          loop={loopState} 
+          id={`${currentSeason}-bgVideo`} 
+          className="bgVideo" 
+          src={this.state.video}
+          autoPlay 
+          muted
+        >
+          <source id={`${currentSeason}-id`} type="video/mp4" />
+        </video>
+
+        <div className="season-page-message">
+          <span className="message-min-text">{message}</span>
+          <div className="season-page-buttons-wrapper">
+            <p>Check out other seasons</p>
+            {this.addButtons(currentSeason)}
+          </div>
         </div>
       </div>
-    </div>
 
-  );
+    );
+  }
 }
 
 export default SeasonPage;
